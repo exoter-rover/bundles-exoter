948f2d03f0abebcf627d008907325d06530bf2d8
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-08-26 10:31:47 +0200

    script: updated gp_odometry log process script

--- scripts/replay/process_logs_threed_with_gp_odometry.rb
+++ scripts/replay/process_logs_threed_with_gp_odometry.rb
@@ -72,6 +72,9 @@ Bundles.run 'exoter_control',
     exoter_odometry = Orocos.name_service.get 'exoter_odometry'
     gp_odometry = Orocos.name_service.get 'gp_odometry'
 
+    # Get the task names from localization
+    msc_localization = Orocos.name_service.get 'msc_localization'
+
     # Set configuration files for control
     Orocos.conf.apply(read_joint_dispatcher, ['reading'], :override => true)
     Orocos.conf.apply(ptu_control, ['default'], :override => true)
@@ -81,7 +84,12 @@ Bundles.run 'exoter_control',
     Orocos.conf.apply(exoter_odometry, ['default', 'bessel50'], :override => true)
     exoter_odometry.urdf_file = Bundles.find_file('data/odometry', 'exoter_odometry_model_complete.urdf')
     Orocos.conf.apply(gp_odometry, ['gp_sklearn'], :override => true)
-    gp_odometry.gaussian_process_x_axis_file = Bundles.find_file('data/gaussian_processes', 'gp_sklearn_x_velocity.data')
+    gp_odometry.gaussian_process_x_axis_file = Bundles.find_file('data/gaussian_processes', 'gp_sklearn_x_delta_pose.data')
+    gp_odometry.gaussian_process_y_axis_file = Bundles.find_file('data/gaussian_processes', 'gp_sklearn_y_delta_pose.data')
+    gp_odometry.gaussian_process_z_axis_file = Bundles.find_file('data/gaussian_processes', 'gp_sklearn_z_delta_pose.data')
+
+    # Set configuration files for localization
+    Orocos.conf.apply(msc_localization, ['default', 'no_update', 'bumblebee_mono_noise'], :override => true)
 
     # logs files
     log_replay = Orocos::Log::Replay.open( logfiles_path )
@@ -90,6 +98,7 @@ Bundles.run 'exoter_control',
     ## TRANSFORMER ##
     #################
     Bundles.transformer.setup(localization_frontend)
+    Bundles.transformer.setup(msc_localization)
 
     ###################
     ## LOG THE PORTS ##
@@ -104,6 +113,9 @@ Bundles.run 'exoter_control',
     localization_frontend.configure
     exoter_odometry.configure
     gp_odometry.configure
+
+    # Configure tasks from localization
+    msc_localization.configure
 
     ###########################
     ## LOG PORTS CONNECTIONS ##
@@ -147,6 +159,9 @@ Bundles.run 'exoter_control',
     localization_frontend.joints_samples_out.connect_to gp_odometry.joints_samples
     localization_frontend.orientation_samples_out.connect_to gp_odometry.orientation_samples
 
+
+    gp_odometry.delta_pose_samples_out.connect_to msc_localization.delta_pose_samples,  :type => :buffer, :size => 1000
+
     # Start tasks from control
     read_joint_dispatcher.start
     ptu_control.start
@@ -155,6 +170,10 @@ Bundles.run 'exoter_control',
     localization_frontend.start
     exoter_odometry.start
     gp_odometry.start
+
+    # Start tasks for localization
+    msc_localization.start
+
 
     # open the log replay widget
     control = Vizkit.control log_replay
