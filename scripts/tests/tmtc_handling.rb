20d1a4d1223366f06e23a2ecb54ff036dc9433f1
exoter <exoter@esa.int>
2016-08-29 12:12:39 +0200

    set the imu config back to ESTEC

--- scripts/tests/tmtc_handling.rb
+++ scripts/tests/tmtc_handling.rb
@@ -177,7 +177,7 @@ Orocos::Process.run 'exoter_control', 'exoter_proprioceptive', 'exoter_groundtru
     # STIM300 IMU
     puts "Setting up imu_stim300"
     imu_stim300 = TaskContext.get 'imu_stim300'
-    Orocos.conf.apply(imu_stim300,  ['default','exoter','SEROM','stim300_5g'], :override => true)
+    Orocos.conf.apply(imu_stim300,  ['default','exoter','ESTEC','stim300_5g'], :override => true)
     imu_stim300.configure
     puts "done"
 
