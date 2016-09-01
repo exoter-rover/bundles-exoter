318ee3d9a4038c726e16e57784bade96ed8e3331
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2016-09-01 20:15:24 +0200

    scripts: update ruby script for the orb_slam2 and 3d_odometry with gp residuals

--- scripts/replay/process_logs_threed_with_gp_odometry.rb
+++ scripts/replay/process_logs_threed_with_gp_odometry.rb
@@ -5,17 +5,19 @@ require 'orocos/log'
 require 'rock/bundle'
 require 'vizkit'
 require 'utilrb'
+require 'optparse'
 
 include Orocos
 
 options = {}
 options[:reference] = "none"
 options[:imu] = "new"
+options[:gaussian_process] = "gpy"
 options[:reaction_forces] = false
 
 op = OptionParser.new do |opt|
     opt.banner = <<-EOD
-    usage: process_logs_threed_odometry [options] <data_log_directory>
+    usage: process_logs_threed_with_gp_odometry [options] <data_log_directory>
     EOD
 
     opt.on "-r", "--reference=none/vicon/gnss", String, 'set the type of reference system available' do |reference|
@@ -28,6 +30,10 @@ op = OptionParser.new do |opt|
 
     opt.on "-f", "--reaction_forces", String, 'connect the reaction forces for the 3D Odometry' do
         options[:reaction_forces] = true
+    end
+
+    opt.on "-g", "--gaussian_process=sklearn/gpy", String, 'Type of the Gaussian process.' do |gp|
+        options[:gaussian_process] = gp
     end
 
     opt.on '--help', 'this help message' do
@@ -59,9 +65,19 @@ else
 end
 
 if options[:imu].casecmp("old").zero?
-    puts "[INFO] Old type of IMU samples in logs"
+    puts "[INFO] Old type of IMU samples from logs"
+elsif options[:imu].casecmp("new").zero?
+    puts "[INFO] New type of IMU samples from logs"
+elsif options[:imu].casecmp("last").zero?
+    puts "[INFO] Last type of IMU samples from logs"
 else
-    puts "[INFO] New type of IMU samples in logs"
+    puts "[INFO] IKF orientation from logs"
+end
+
+if options[:gaussian_process].casecmp("sklearn").zero?
+    puts "[INFO] Selected Sklearn Gaussian process"
+elsif options[:gaussian_process].casecmp("gpy").zero?
+    puts "[INFO] Selected GPy Gaussian process"
 end
 
 if options[:reaction_forces]
@@ -70,9 +86,14 @@ else
     puts "[INFO] 3D Odometry without reaction forces enhancement"
 end
 
-Bundles.run 'exoter_control',
-            'exoter_localization',
-            :gdb => false do
+Bundles.run 'joint_dispatcher::Task' => 'read_joint_dispatcher',
+            'ptu_control::Task' => 'ptu_control',
+            'localization_frontend::Task' => 'localization_frontend',
+            'threed_odometry::Task' => 'exoter_odometry',
+            'gp_odometry::SklearnTask' => 'sklearn_gp_odometry',
+            'gp_odometry::GpyTask' => 'gpy_gp_odometry',
+            :gdb => false,
+            :output => nil do
 
     # Get the task names from control
     read_joint_dispatcher = Orocos.name_service.get 'read_joint_dispatcher'
@@ -81,10 +102,12 @@ Bundles.run 'exoter_control',
     # Get the task names from odometry
     localization_frontend = Orocos.name_service.get 'localization_frontend'
     exoter_odometry = Orocos.name_service.get 'exoter_odometry'
-    gp_odometry = Orocos.name_service.get 'gp_odometry'
 
-    # Get the task names from localization
-    #msc_localization = Orocos.name_service.get 'msc_localization'
+    if options[:gaussian_process].casecmp("sklearn").zero?
+        gp_odometry = Orocos.name_service.get 'sklearn_gp_odometry'
+    elsif options[:gaussian_process].casecmp("gpy").zero?
+        gp_odometry = Orocos.name_service.get 'gpy_gp_odometry'
+    end
 
     # Set configuration files for control
     Orocos.conf.apply(read_joint_dispatcher, ['reading'], :override => true)
@@ -94,13 +117,17 @@ Bundles.run 'exoter_control',
     Orocos.conf.apply(localization_frontend, ['default', 'hamming1hzsampling12hz'], :override => true)
     Orocos.conf.apply(exoter_odometry, ['default', 'bessel50'], :override => true)
     exoter_odometry.urdf_file = Bundles.find_file('data/odometry', 'exoter_odometry_model_complete.urdf')
-    Orocos.conf.apply(gp_odometry, ['gp_sklearn'], :override => true)
-    gp_odometry.gaussian_process_x_axis_file = Bundles.find_file('data/gaussian_processes', 'gp_sklearn_x_delta_pose.data')
-    gp_odometry.gaussian_process_y_axis_file = Bundles.find_file('data/gaussian_processes', 'gp_sklearn_y_delta_pose.data')
-    gp_odometry.gaussian_process_z_axis_file = Bundles.find_file('data/gaussian_processes', 'gp_sklearn_z_delta_pose.data')
 
-    # Set configuration files for localization
-    #Orocos.conf.apply(msc_localization, ['default', 'no_update'], :override => true)
+    if options[:gaussian_process].casecmp("sklearn").zero?
+        Orocos.conf.apply(gp_odometry, ['gp_sklearn'], :override => true)
+        gp_odometry.gaussian_process_x_axis_file = Bundles.find_file('data/gaussian_processes', 'gp_sklearn_x_delta_pose.data')
+        gp_odometry.gaussian_process_y_axis_file = Bundles.find_file('data/gaussian_processes', 'gp_sklearn_y_delta_pose.data')
+        gp_odometry.gaussian_process_z_axis_file = Bundles.find_file('data/gaussian_processes', 'gp_sklearn_z_delta_pose.data')
+    elsif options[:gaussian_process].casecmp("gpy").zero?
+        Orocos.conf.apply(gp_odometry, ['gp_gpy'], :override => true)
+        gp_odometry.path_to_init = File.join(Rock::Bundles.current_bundle.path, 'data', 'gaussian_processes')
+        gp_odometry.gaussian_process_file = Bundles.find_file('data/gaussian_processes', 'SparseGP_RBF_xyz_velocities_train_at_500ms.data')
+    end
 
     # logs files
     log_replay = Orocos::Log::Replay.open( logfiles_path )
@@ -109,7 +136,7 @@ Bundles.run 'exoter_control',
     ## TRANSFORMER ##
     #################
     Bundles.transformer.setup(localization_frontend)
-    #Bundles.transformer.setup(msc_localization)
+    Bundles.transformer.setup(exoter_odometry)
 
     ###################
     ## LOG THE PORTS ##
@@ -125,13 +152,18 @@ Bundles.run 'exoter_control',
     exoter_odometry.configure
     gp_odometry.configure
 
-    # Configure tasks from localization
-    #msc_localization.configure
-
     ###########################
     ## LOG PORTS CONNECTIONS ##
     ###########################
     log_replay.platform_driver.joints_readings.connect_to(read_joint_dispatcher.joints_readings, :type => :buffer, :size => 200)
+
+    if options[:reference].casecmp("vicon").zero?
+        log_replay.vicon.pose_samples.connect_to(localization_frontend.pose_reference_samples, :type => :buffer, :size => 200)
+    end
+
+    if options[:reference].casecmp("gnss").zero?
+        log_replay.gnss_trimble.pose_samples.connect_to(localization_frontend.pose_reference_samples, :type => :buffer, :size => 200)
+    end
 
     if options[:imu].casecmp("old").zero?
         log_replay.stim300.orientation_samples_out.connect_to(localization_frontend.orientation_samples, :type => :buffer, :size => 200)
@@ -147,15 +179,6 @@ Bundles.run 'exoter_control',
         log_replay.imu_stim300.orientation_samples_out.connect_to(localization_frontend.orientation_samples, :type => :buffer, :size => 200)
         log_replay.imu_stim300.compensated_sensors_out.connect_to(localization_frontend.inertial_samples, :type => :buffer, :size => 200)
     end
-
-    if options[:reference].casecmp("vicon").zero?
-        log_replay.vicon.pose_samples.connect_to(localization_frontend.pose_reference_samples, :type => :buffer, :size => 200)
-    end
-
-    if options[:reference].casecmp("gnss").zero?
-        log_replay.gnss_trimble.pose_samples.connect_to(localization_frontend.pose_reference_samples, :type => :buffer, :size => 200)
-    end
-
 
     #############################
     ## TASKS PORTS CONNECTIONS ##
@@ -174,7 +197,9 @@ Bundles.run 'exoter_control',
     localization_frontend.joints_samples_out.connect_to gp_odometry.joints_samples, :type => :buffer, :size => 200
     localization_frontend.orientation_samples_out.connect_to gp_odometry.orientation_samples, :type => :buffer, :size => 200
 
-    #gp_odometry.delta_pose_samples_out.connect_to msc_localization.delta_pose_samples,  :type => :buffer, :size => 200
+    if options[:gaussian_process].casecmp("gpy").zero?
+        localization_frontend.inertial_samples_out.connect_to gp_odometry.inertial_samples, :type => :buffer, :size => 200
+    end
 
     # Start tasks from control
     read_joint_dispatcher.start
@@ -184,10 +209,6 @@ Bundles.run 'exoter_control',
     localization_frontend.start
     exoter_odometry.start
     gp_odometry.start
-
-    # Start tasks for localization
-    #msc_localization.start
-
 
     # open the log replay widget
     control = Vizkit.control log_replay
