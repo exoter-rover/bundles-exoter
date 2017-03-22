041e6a6a6b185a1dbd0cf251a7a0b9d749330f58
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2017-03-22 18:53:05 +0100

    config: SARGON version for localization_frontend scripts: test to run 3d odometry using the SARGON version of localization frontend.

---
+++ scripts/replay/process_logs_threed_odometry_sargon.rb
@@ -0,0 +1,137 @@
+#!/usr/bin/env ruby
+
+require 'orocos'
+require 'orocos/log'
+require 'rock/bundle'
+require 'vizkit'
+require 'utilrb'
+require 'optparse'
+
+include Orocos
+
+options = {}
+options[:imu] = "new"
+
+op = OptionParser.new do |opt|
+    opt.banner = <<-EOD
+    usage: process_logs_threed_odometry [options] <data_log_directory>
+    EOD
+
+    opt.on "-i", "--imu=old/new/last", String, 'since the imu component changed. Please set the type' do |imu|
+        options[:imu] = imu
+    end
+
+    opt.on '--help', 'this help message' do
+        puts opt
+       exit 0
+    end
+end
+
+args = op.parse(ARGV)
+logfiles_path = args.shift
+
+if !logfiles_path
+    puts "missing path to log files"
+    puts options
+    exit 1
+end
+
+Orocos::CORBA::max_message_size = 100000000000
+Bundles.initialize
+Bundles.transformer.load_conf(Bundles.find_file('config', 'transforms_scripts_exoter_odometry.rb'))
+
+if options[:imu].casecmp("old").zero?
+    puts "[INFO] Old type of IMU samples in logs"
+else
+    puts "[INFO] New type of IMU samples in logs"
+end
+
+Bundles.run 'joint_dispatcher::Task' => 'read_joint_dispatcher',
+            'ptu_control::Task' => 'ptu_control',
+            'localization_frontend::Task' => 'localization_frontend',
+            'threed_odometry::Task' => 'exoter_odometry',
+            :gdb => false,
+            :output => nil do
+
+    # Get the task names from control
+    read_joint_dispatcher = Orocos.name_service.get 'read_joint_dispatcher'
+    ptu_control = Orocos.name_service.get 'ptu_control'
+
+    # Get the task names from odometry
+    localization_frontend = Orocos.name_service.get 'localization_frontend'
+    exoter_odometry = Orocos.name_service.get 'exoter_odometry'
+
+    # Set configuration files for control
+    Orocos.conf.apply(read_joint_dispatcher, ['reading'], :override => true)
+    Orocos.conf.apply(ptu_control, ['default'], :override => true)
+
+    # Set configuration files for odometry
+    Orocos.conf.apply(localization_frontend, ['default', 'hamming1hzsampling12hz'], :override => true)
+    localization_frontend.urdf_file = Bundles.find_file('data/odometry', 'exoter_odometry_model_complete.urdf')
+    Orocos.conf.apply(exoter_odometry, ['default', 'bessel50'], :override => true)
+    exoter_odometry.urdf_file = Bundles.find_file('data/odometry', 'exoter_odometry_model_complete.urdf')
+
+    # logs files
+    log_replay = Orocos::Log::Replay.open( logfiles_path )
+
+    #################
+    ## TRANSFORMER ##
+    #################
+    Bundles.transformer.setup(exoter_odometry)
+
+    ###################
+    ## LOG THE PORTS ##
+    ###################
+    Bundles.log_all
+
+    # Configure tasks from control
+    read_joint_dispatcher.configure
+    ptu_control.configure
+
+    # Configure tasks from odometry
+    localization_frontend.configure
+    exoter_odometry.configure
+
+    ###########################
+    ## LOG PORTS CONNECTIONS ##
+    ###########################
+    log_replay.platform_driver.joints_readings.connect_to(read_joint_dispatcher.joints_readings, :type => :buffer, :size => 200)
+
+    if options[:imu].casecmp("old").zero?
+        log_replay.stim300.orientation_samples_out.connect_to(localization_frontend.orientation_samples, :type => :buffer, :size => 200)
+        log_replay.stim300.calibrated_sensors.connect_to(localization_frontend.inertial_samples, :type => :buffer, :size => 200)
+    end
+
+    if options[:imu].casecmp("new").zero?
+        log_replay.imu_stim300.orientation_samples_out.connect_to(localization_frontend.orientation_samples, :type => :buffer, :size => 200)
+        log_replay.imu_stim300.calibrated_sensors.connect_to(localization_frontend.inertial_samples, :type => :buffer, :size => 200)
+    end
+
+    if options[:imu].casecmp("last").zero?
+        log_replay.imu_stim300.orientation_samples_out.connect_to(localization_frontend.orientation_samples, :type => :buffer, :size => 200)
+        log_replay.imu_stim300.compensated_sensors_out.connect_to(localization_frontend.inertial_samples, :type => :buffer, :size => 200)
+    end
+
+    #############################
+    ## TASKS PORTS CONNECTIONS ##
+    #############################
+
+    read_joint_dispatcher.joints_samples.connect_to localization_frontend.joints_samples, :type => :buffer, :size => 200
+    read_joint_dispatcher.ptu_samples.connect_to ptu_control.ptu_samples, :type => :buffer, :size => 200
+    localization_frontend.joints_samples_out.connect_to exoter_odometry.joints_samples, :type => :buffer, :size => 200
+    localization_frontend.orientation_samples_out.connect_to exoter_odometry.orientation_samples, :type => :buffer, :size => 200
+
+    # Start tasks from control
+    read_joint_dispatcher.start
+    ptu_control.start
+
+    # Start tasks from slam
+    localization_frontend.start
+    exoter_odometry.start
+
+    # open the log replay widget
+    control = Vizkit.control log_replay
+    control.speed = 1
+
+    Vizkit.exec
+end
